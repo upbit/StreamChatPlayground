4d251cad2a1958bc4acf23a4e18914035cdae1a0
Hao Zhou <rmusique@gmail.com>
2024-04-11 15:52:20 +0800

    feat: Add pdf OCR with deepdoc

---
+++ notebooks/pdf_utils/deepdoc/__init__.py
@@ -0,0 +1,5 @@
+from .ocr import OCR
+from .recognizer import Recognizer
+from .table_structure_recognizer import TableStructureRecognizer
+
+__all__ = ["OCR", "Recognizer", "TableStructureRecognizer"]
